3af6610d50ca0dfdd5e5698a8a168144b61f2173
Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>
2015-06-16 10:20:08 +0200

    gdb-arm.sh
    Signed-off-by: Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>

--- gdb-arm.sh
+++ gdb-arm.sh
@@ -3,6 +3,7 @@
 ELF_PATH="$1"
 CFG_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# Provide a way to overwrite default gdb path
 if [ -f "$CFG_PATH"/config.sh ]; then
 	source "$CFG_PATH/config.sh"
 else
