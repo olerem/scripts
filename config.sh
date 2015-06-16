644654f18e03cf0017f7d0044724265b5b1c11bd
Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>
2015-06-16 10:17:40 +0200

    add config.sh
    Signed-off-by: Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PRE_PATH="/opt/tooling/codesourcery/MGC-2014.05-46-arm-gcc-4.8.3/Sourcery_CodeBench_for_ARM_Embedded/bin"
+PATH=$PRE_PATH:$PATH
+GDB="arm-none-linux-gnueabi-gdb"
--- gdb-arm.sh
+++ gdb-arm.sh
@@ -3,6 +3,12 @@
 ELF_PATH="$1"
 CFG_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-~/tmp/gdb-7.9/gdb/gdb -nx -ex "source "$CFG_PATH"/gdb-openocd-base.ini" \
+if [ -f "$CFG_PATH"/config.sh ]; then
+	source "$CFG_PATH/config.sh"
+else
+	GDB="gdb"
+fi
+
+"$GDB" -nx -ex "source "$CFG_PATH"/gdb-openocd-base.ini" \
 	-ex "source "$CFG_PATH"/gdb-openocd-smp.ini" \
 	"$ELF_PATH"
