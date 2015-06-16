2f123b67104cfa550935771f8d89e79dee515488
Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>
2015-06-16 09:53:19 +0200

    gdb-arm.sh
    Signed-off-by: Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>

--- gdb-arm.sh
+++ gdb-arm.sh
@@ -4,4 +4,5 @@ ELF_PATH="$1"
 CFG_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ~/tmp/gdb-7.9/gdb/gdb -nx -ex "source "$CFG_PATH"/gdb-openocd-base.ini" \
+	-ex "source "$CFG_PATH"/gdb-openocd-smp.ini" \
 	"$ELF_PATH"
