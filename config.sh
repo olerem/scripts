29f71dcb7e76107265671848ce07f18ed58bb69a
Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>
2015-07-14 08:53:34 +0200

    config.sh
    Signed-off-by: Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>

--- config.sh
+++ config.sh
@@ -2,4 +2,5 @@
 
 PRE_PATH="/opt/tooling/codesourcery/MGC-2014.05-46-arm-gcc-4.8.3/Sourcery_CodeBench_for_ARM_Embedded/bin"
 PATH=$PRE_PATH:$PATH
-GDB="arm-none-linux-gnueabi-gdb"
+#GDB="arm-none-linux-gnueabi-gdb"
+GDB="/home/rem1hi/tmp/gdb-7.9/gdb/gdb"
